871d0f423efa3604903cb0b65eb513195f1c1bf1
JohnGuillermo217CS <johnguillermo217@gmail.com>
2024-09-30 15:56:22 -0700

    Add files via upload

---
+++ src/api/barrels.py
@@ -0,0 +1,61 @@
+import sqlalchemy
+from src import database as db
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from src.api import auth
+
+router = APIRouter(
+    prefix="/barrels",
+    tags=["barrels"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+class Barrel(BaseModel):
+    sku: str
+
+    ml_per_barrel: int
+    potion_type: list[int]
+    price: int
+
+    quantity: int
+
+@router.post("/deliver/{order_id}")
+def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
+    """ """
+    print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+
+    return "OK"
+
+# Gets called once a day
+@router.post("/plan")
+def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
+    """ """
+    print(wholesale_catalog)
+
+    with db.engine.begin() as connection:
+        numGreenPots = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
+    with db.engine.begin() as connection:
+        gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar()
+
+    barrelsToBuy = (int)(gold / 100)
+    if barrelsToBuy > 10:
+        barrelsToBuy = 10
+
+    if numGreenPots < 10:
+        for i in range(barrelsToBuy):
+            with db.engine.begin() as connection:
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + 500"))
+            with db.engine.begin() as connection:
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - 100"))
+        return [
+            {
+                "sku": "SMALL_GREEN_BARREL",
+                "quantity": barrelsToBuy,
+            }
+        ]
+    else:
+        return
+
+
+    
+
