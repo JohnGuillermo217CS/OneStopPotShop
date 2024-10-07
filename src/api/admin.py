37d81cd6eaa4de4b08710bae7f6a7a2c0f54be2a
JohnGuillermo217CS <johnguillermo217@gmail.com>
2024-10-07 14:49:17 -0700

    Add files via upload

---
+++ src/api/admin.py
@@ -0,0 +1,29 @@
+import sqlalchemy
+from src import database as db
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+@router.post("/reset")
+def reset():
+    """
+    Reset the game state. Gold goes to 100, all potions are removed from
+    inventory, and all barrels are removed from inventory. Carts are all reset.
+    """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = 0"))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = 0"))
+        
+    return "OK"
+
